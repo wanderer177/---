09a0f36d4e71e39035ea6973cb02c1ccbd172191
wanderer177 <1216005040@qq.com>
2024-05-18 11:30:51 +0800

    提交文件

---
+++ weapp-wechat-zhihu-master/data/data_discovery.js
@@ -0,0 +1,88 @@
+/**
+ * Created by Rebecca_Han on 16/10/26.
+ */
+module.exports = {
+
+}
+
+var discovery= {
+    "id": 3,
+        "data": [
+        {
+            "question_id": 1,
+            "answer_id": 3,
+            "feed_source_id": 23,
+            "feed_source_name": "Rebecca1",
+            "feed_source_txt": "赞了回答1",
+            "feed_source_img": "../../images/icon1.jpeg",
+            "question": "家庭有机种植经验分享",
+            "answer_ctnt": "大家好，我是村里的小李。这几年我一直在尝试有机种植，避免使用化学肥料和农药，效果不错。主要做法是使用自制的堆肥和鸡粪作肥料，并且每年轮作不同的蔬菜，以保持土壤肥力。如果大家有兴趣，可以来我家看看，互相交流经验，一起学习。",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 2,
+            "answer_id": 25,
+            "feed_source_id": 24,
+            "feed_source_name": "Alex2",
+            "feed_source_txt": "回答了问题2",
+            "feed_source_img": "../../images/icon8.jpg",
+            "question": "为什么我家的蔬菜虫害很严重呢？",
+            "answer_ctnt": "我家也种了蔬菜，但是虫害较少，我的建议是，可以使用一些天然方法来控制虫害，如喷洒大蒜水或辣椒水。另外，种植一些驱虫植物，如薄荷、香菜，也能有效减少虫害。还可以采用物理方法，如覆盖纱网，防止害虫接触蔬菜。",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 3,
+            "answer_id": 61,
+            "feed_source_id": 25,
+            "feed_source_name": "George3",
+            "feed_source_txt": "赞了回答3",
+            "feed_source_img": "../../images/icon9.jpeg",
+            "question": "有机种植经验分享",
+            "answer_ctnt": "有机农业(organic agriculture)遵照特定的农业生产原则，在生产中不采用基因工程获得的生物及其产物，不使用化学合成的农药、化肥、生长调节剂、饲料添加剂等物质，遵循自然规律和生态学原理，协调种植业和养殖业的平衡，采用一系列可持续发展的农业技术以维持持续稳定的农业生产体系的一种农业生产方式。总之，有机农业体系强调依赖于自然和生物之间的循环，即农业中的种植业和养殖业的平衡来实现可持续的投入-产出。因此，做有机农业不用化学投入品，重点是在'建立有机农业体系'，而不是单纯的'不使用化肥、农药'。在得到认证的有农业体系之下生产的农产品，才能够被称为'有机农产品'。 ",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 4,
+            "answer_id": 3,
+            "feed_source_id": 23,
+            "feed_source_name": "Rebecca4",
+            "feed_source_txt": "赞了回答4",
+            "feed_source_img": "../../images/icon1.jpeg",
+            "question": "农机共享计划，帮助大家提高效率！",
+            "answer_ctnt": "大家好，我是小王。为了更好地利用农机资源，提高大家的农业生产效率，我们决定和大家共享我家的农机。以下是计划细节：共享农机：耕地机。使用方式：按需预约，每小时收费合理。优先满足急需。预约方式：请联系小王，电话123-456-7890。注意事项：使用前请检查农机状态，使用后请及时清理和维护。若有损坏，请及时报告。 欢迎大家积极参与，互帮互助，共同提高生产效益！如有疑问或建议，请随时联系我。联系信息：小王，电话：123-456-7890 ",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 5,
+            "answer_id": 25,
+            "feed_source_id": 24,
+            "feed_source_name": "Alex5",
+            "feed_source_txt": "回答了问题5",
+            "feed_source_img": "../../images/icon8.jpg",
+            "question": "我家的果树产量怎么那么不稳定？一年高一年低的",
+            "answer_ctnt": "果树产量不稳定可能与修剪不当、施肥不合理、病虫害有关。要定期修剪果树，促进新枝生长；合理施肥，尤其是开花和结果期；定期检查果树，及时防治病虫害。",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 6,
+            "answer_id": 61,
+            "feed_source_id": 25,
+            "feed_source_name": "George6",
+            "feed_source_txt": "赞了回答6",
+            "feed_source_img": "../../images/icon9.jpeg",
+            "question": "开发乡村旅游的经验和招募更多村民 ",
+            "answer_ctnt": "大家好，我是小陈。我们村有很好的自然景观和民俗文化，去年开始尝试开发乡村旅游，效果不错。主要是整合村里的资源，提供农家乐、手工艺体验和自然观光服务。我真心希望有更多的村民参与进来，共同致富。欢迎有兴趣的朋友一起探讨。",
+            "good_num": "112",
+            "comment_num": "18"
+        }
+
+    ]
+
+}
+
+module.exports.discovery = discovery;
